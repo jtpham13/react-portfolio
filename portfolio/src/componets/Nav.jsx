35364cbed439534b582ac7edd8eb5e8374494dc1
Joseph Pham <joeypham13@gmail.com>
2023-08-10 22:42:14 -0400

    added componets and pages folder

---
+++ portfolio/src/componets/Nav.jsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return <ul>
+        <li><a href="#">About me</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Resume</a></li>
+    </ul>
+}
+
+export default Nav
