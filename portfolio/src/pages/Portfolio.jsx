e4f5beec88c1e39ff99d47be2eacb154cb3e1745
Joseph Pham <joeypham13@gmail.com>
2023-08-12 16:40:02 -0400

    added portfolio array

--- portfolio/src/pages/Portfolio.jsx
+++ portfolio/src/pages/Portfolio.jsx
@@ -1,5 +1,30 @@
-function Portfolio(){
-    return<h1>Portfolio</h1>
-}
+import Image from "../assets/portfolio1.png"
 
+function Portfolio(){
+    const projs = [
+      {
+        name: "Proj 1",
+        link: "https://www.google.com/",
+        image: Image,
+      }
+    ]
+  
+    return (
+      <section>
+        <h2>Projects</h2>
+        <ul>
+        {
+          projs.map((project, i) => (
+            <li key = {i}>
+              <a href={project.link}>
+                {project.name}
+              </a>
+              <img src={project.image}/>
+            </li>
+          ))
+        }
+        </ul>
+      </section>
+    )
+  };
 export default Portfolio;
